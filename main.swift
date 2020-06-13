f9a63f7a71093577de399d9a947b4378bc393a41
Jim Eckerlein <jim.eckerlein@gmail.com>
2020-06-13 13:52:27 +0000

    add main.swift

---
+++ main.swift
@@ -0,0 +1,54 @@
+import AppKit
+
+var running = true
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        NSLog("start app")
+    }
+    
+    func applicationWillTerminate(_ notification: Notification) {
+        NSLog("applicationWillTerminate")
+        running = false
+    }
+}
+
+class WindowDelegate: NSObject, NSWindowDelegate {
+    func windowDidResize(_ notification: Notification) {
+        NSLog("windowDidResize")
+    }
+    
+    func windowWillClose(_ notification: Notification) {
+        NSLog("windowWillClose")
+        running = false
+    }
+}
+
+let app = NSApplication.shared
+let appDelegate = AppDelegate()
+app.delegate = appDelegate
+app.setActivationPolicy(.regular)
+app.finishLaunching()
+
+let window = NSWindow(contentRect: NSMakeRect(0, 0, 1024, 768),
+                      styleMask: [.closable, .titled, .resizable, .miniaturizable],
+                      backing: .buffered,
+                      defer: true)
+
+let windowDelegate = WindowDelegate()
+window.delegate = windowDelegate
+window.title = "Hey, Window under control!"
+window.acceptsMouseMovedEvents = true
+window.center()
+window.orderFrontRegardless()
+
+while(running) {
+    var ev: NSEvent?
+    ev = app.nextEvent(matching: .any, until: nil, inMode: .default, dequeue: true)
+    if (ev != nil) {
+        NSLog("%@", ev!)
+        app.sendEvent(ev!)
+    }
+}
+
+app.terminate(nil)
